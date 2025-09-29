fbb8240a27e75ac657528bb3224c53d3eaca3dd8
D. Sohan Kumar <sohans1092004@gmail.com>
2025-09-30 00:51:31 +0530

    more errors corrected

--- app/(public)/(routes)/preview/[documentId]/page.tsx
+++ app/(public)/(routes)/preview/[documentId]/page.tsx
@@ -2,9 +2,10 @@ import { Id } from "@/convex/_generated/dataModel";
 import Client from "./client";
 
 interface DocumentIdPageProps {
-  params: { documentId: Id<"documents"> };
+  params: Promise<{ documentId: Id<"documents"> }>;
 }
 
-export default function DocumentIdPage({ params }: DocumentIdPageProps) {
-  return <Client documentId={params.documentId} />;
+export default async function DocumentIdPage({ params }: DocumentIdPageProps) {
+  const { documentId } = await params;
+  return <Client documentId={documentId} />;
 }
